cf2b2960efba1631499ca90d5b5bb0fdf8f30eff
Kashyap2701 <kspatelsimform100@gmail.com>
2024-09-23 14:16:58 +0530

    initial setup

---
+++ apps/auth/src/app/app.tsx
@@ -0,0 +1,11 @@
+import '../styles.css';
+
+export function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Auth App</h1>
+    </div>
+  );
+}
+
+export default App;
