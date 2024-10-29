d83f3150dbfde4890f9d60fc6fb45b8970c51505
Vatsal Dave <vatsaldave2002@gmail.com>
2024-10-29 12:10:43 +0530

    feat: sm-123 - add chat mfe types

---
+++ shared/api/src/types/chat-types.ts
@@ -0,0 +1,82 @@
+import { PaginatedResponse, QueryPagination } from './utils-types';
+
+export interface ChatUser {
+  id: string;
+  name: string;
+  profilePicture: string;
+  isDeleted: boolean;
+}
+
+export interface Message {
+  id: string;
+  content: string;
+  senderId: string;
+  oneOnOneChatId: string;
+  groupChatId: null;
+  createdAt: string;
+  updatedAt: string;
+  isDeleted: boolean;
+  sender: ChatUser;
+}
+
+export interface OneOnOneChat {
+  id: string;
+  initiatorId: string;
+  participantId: string;
+  vanishMode: boolean;
+  createdAt: string;
+  updatedAt: string;
+  lastMessageAt: string;
+  // "deletedForInitiator": null,
+  // "deletedForParticipant": null,
+}
+
+export interface OneOnOneChatSettings {
+  vanishMode: boolean;
+}
+
+export interface GroupChat {
+  id: string;
+  name: string;
+  ownerId: string;
+  groupDescription: string;
+  groupIcon: string;
+  createdAt: string;
+  updateAt: string;
+  lastMessageAt: string;
+  memberIds: string[];
+}
+
+export interface GroupChatSettings {
+  name?: string;
+  groupDescription?: string;
+  groupIcon?: File | undefined;
+}
+
+export interface IChatServices {
+  getOneOnOneChat(chatId: string): Promise<OneOnOneChat>;
+  updateOneOnOneChatSettings(
+    chatId: string,
+    settings: OneOnOneChatSettings
+  ): Promise<string>;
+  getOneOnOneChatMessages(
+    chatId: string,
+    params?: QueryPagination
+  ): Promise<PaginatedResponse<'messages', Message[]>>;
+  getGroupChat(chatId: string): Promise<GroupChat>;
+  updateGroupChat(chatId: string, settings: GroupChatSettings): Promise<string>;
+  getGroupChatMessages(
+    chatId: string,
+    params?: QueryPagination
+  ): Promise<PaginatedResponse<'messages', Message[]>>;
+  addGroupChatMembers(
+    chatId: string,
+    ownerId: string,
+    memberIds: string[]
+  ): Promise<string>;
+  removeGroupChatMembers(
+    chatId: string,
+    ownerId: string,
+    memberId: string
+  ): Promise<string>;
+}
