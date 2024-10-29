19867fdca437d29403f7a39c0504d8853c1dc181
Vatsal Dave <vatsaldave2002@gmail.com>
2024-10-29 12:11:12 +0530

    feat: sm-124 - add chat mfe services

---
+++ shared/api/src/services/chat-services.ts
@@ -0,0 +1,111 @@
+import apiClient from '../axios/axios-instance';
+import { PaginatedResponse, QueryPagination } from '../types';
+import {
+  GroupChat,
+  GroupChatSettings,
+  IChatServices,
+  Message,
+  OneOnOneChat,
+  OneOnOneChatSettings,
+} from '../types/chat-types';
+
+const ONE_ON_ONE_CHAT_ENDPOINT = '/chats/one-on-one-chats';
+const GROUP_CHAT_ENDPOINT = '/chats/groups';
+
+export const getOneOnOneChat: IChatServices['getOneOnOneChat'] = async (
+  chatId
+) => {
+  const { data } = await apiClient.get<OneOnOneChat>(
+    `${ONE_ON_ONE_CHAT_ENDPOINT}/${chatId}`
+  );
+  return data;
+};
+
+export const updateOneOnOneChatSettings: IChatServices['updateOneOnOneChatSettings'] =
+  async (chatId, settings) => {
+    await apiClient.patch<OneOnOneChatSettings>(
+      `${ONE_ON_ONE_CHAT_ENDPOINT}/${chatId}/settings`,
+      settings
+    );
+
+    return 'Chat settings updated successfully!';
+  };
+
+export const getOneOnOneChatMessages: IChatServices['getOneOnOneChatMessages'] =
+  async (chatId: string, params?: QueryPagination) => {
+    const { query = '', cursor = '', take = '' } = params || {};
+
+    const { data } = await apiClient.get<
+      PaginatedResponse<'messages', Message[]>
+    >(
+      `${ONE_ON_ONE_CHAT_ENDPOINT}/${chatId}/messages?cursor=${cursor}&take=${take}&query=${query}`
+    );
+    return data;
+  };
+
+export const getGroupChat: IChatServices['getGroupChat'] = async (
+  chatId: string
+) => {
+  const { data } = await apiClient.get<GroupChat>(
+    `${GROUP_CHAT_ENDPOINT}/${chatId}`
+  );
+  return data;
+};
+
+export const updateGroupChat: IChatServices['updateGroupChat'] = async (
+  chatId: string,
+  settings: GroupChatSettings
+) => {
+  const { name, groupDescription, groupIcon } = settings;
+  await apiClient.patch<GroupChatSettings>(
+    `${GROUP_CHAT_ENDPOINT}/${chatId}/settings`,
+    {
+      settings: {
+        name,
+        groupDescription,
+      },
+      groupIcon,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    }
+  );
+
+  return 'Chat settings updated successfully!';
+};
+
+export const getGroupChatMessages: IChatServices['getGroupChatMessages'] =
+  async (chatId: string, params?: QueryPagination) => {
+    const { query = '', cursor = '', take = '' } = params || {};
+
+    const { data } = await apiClient.get<
+      PaginatedResponse<'messages', Message[]>
+    >(
+      `${GROUP_CHAT_ENDPOINT}/${chatId}/messages?cursor=${cursor}&take=${take}&query=${query}`
+    );
+    return data;
+  };
+
+export const addGroupChatMembers: IChatServices['addGroupChatMembers'] = async (
+  chatId: string,
+  ownerId: string,
+  memberIds: string[]
+) => {
+  await apiClient.patch(`${GROUP_CHAT_ENDPOINT}/${chatId}/add-members`, {
+    ownerId,
+    memberIds,
+  });
+
+  return 'Members added successfully!';
+};
+
+export const removeGroupChatMembers: IChatServices['removeGroupChatMembers'] =
+  async (chatId: string, ownerId: string, memberId: string) => {
+    await apiClient.patch(`${GROUP_CHAT_ENDPOINT}/${chatId}/remove-member`, {
+      ownerId,
+      memberId,
+    });
+    return 'Member removed successfully!';
+  };
