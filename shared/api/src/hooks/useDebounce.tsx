b76c4ab3c84b80583116c9c2af110a0ef7a6055c
codewithvip <832vipulahir@gmail.com>
2024-10-30 12:15:17 +0530

    feat:add usedebounce hook in shared

--- shared/api/src/hooks/useDebounce.tsx
+++ shared/api/src/hooks/useDebounce.tsx
@@ -1,7 +1,7 @@
 // debounce hook for search
 import { useEffect, useState } from 'react';
 
-function useDebounce<T>(value: T, delay: number): T {
+export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
@@ -16,5 +16,3 @@ function useDebounce<T>(value: T, delay: number): T {
 
   return debouncedValue;
 }
-
-export default useDebounce;
