122117ad553fe15321599e261a87dbd86ef3ceb0
Kashyap2701 <kspatelsimform100@gmail.com>
2024-10-30 11:43:50 +0530

    feat: add profile-hook

---
+++ shared/api/src/hooks/profile-hooks.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  fetchFriendshipStatus,
+  fetchProfile,
+  fetchUser,
+  updateProfile,
+} from '../services/profile-services';
+import { EditUser, UserProfile } from '../types';
+
+export const useProfile = () => {
+  return useQuery({
+    queryKey: ['profile'],
+    queryFn: fetchProfile,
+  });
+};
+
+export const useUser = (userId: string) => {
+  return useQuery({
+    queryKey: ['user', userId],
+    queryFn: () => fetchUser(userId),
+  });
+};
+
+export const useProfileUpdate = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (profileData: EditUser) => updateProfile(profileData),
+    onSuccess: (data: UserProfile) => {
+      queryClient.setQueryData(['profile'], data);
+    },
+  });
+};
+
+export const useFriendshipStatus = (userId: string, friendId: string) => {
+  return useQuery({
+    queryKey: ['friendshipStatus', userId, friendId],
+    queryFn: () => fetchFriendshipStatus(userId, friendId),
+  });
+};
