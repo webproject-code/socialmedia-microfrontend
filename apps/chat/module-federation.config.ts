578224cf7ca323693027414e9859cd2cfb130582
VatsalDave2102 <vatsaldave2002@gmail.com>
2024-11-07 14:09:36 +0530

    fix: zod and socket.io dependency issue

--- apps/chat/module-federation.config.ts
+++ apps/chat/module-federation.config.ts
@@ -6,6 +6,21 @@ const config: ModuleFederationConfig = {
   exposes: {
     './Module': './src/remote-entry.ts',
   },
+  shared: (library, defaultConfig) => {
+    if (library === '@hookform/resolvers/zod') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    if (library === 'socket.io-client') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    return defaultConfig;
+  },
 };
 
 export default config;
