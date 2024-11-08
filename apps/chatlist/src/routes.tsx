71b8f551bf968f213642fb7c84ae1f90a4bd77c7
codewithvip <832vipulahir@gmail.com>
2024-11-08 11:07:56 +0530

    feat:create home page for mfe

--- apps/chatlist/src/routes.tsx
+++ apps/chatlist/src/routes.tsx
@@ -1,32 +1,37 @@
-import { RouteObject, useParams, useSearchParams } from 'react-router-dom';
+import { RouteObject } from 'react-router-dom';
 import { ChatsList } from './pages/chatsList';
+import { Home } from './pages/home';
 
-const ChatMessageArea = () => {
-  const { chatId } = useParams();
-  const [searchParams] = useSearchParams();
-  const type = searchParams.get('type');
-  return (
-    <>
-      chat message area
-      {chatId}
-      {type}
-    </>
-  );
-};
+// const ChatMessageArea = () => {
+//   const { chatId } = useParams();
+//   const [searchParams] = useSearchParams();
+//   const type = searchParams.get('type');
+//   return (
+//     <>
+//       chat message area
+//       {chatId}
+//       {type}
+//     </>
+//   );
+// };
 
 export const routes: RouteObject[] = [
   {
+    path: '/',
+    element: <Home />,
+  },
+  {
     path: '/chats',
     element: <ChatsList />,
-    children: [
-      {
-        path: 'one-on-one/:chatId',
-        element: <ChatMessageArea />,
-      },
-      {
-        path: 'group/:chatId',
-        element: <ChatMessageArea />,
-      },
-    ],
+    // children: [
+    //   {
+    //     path: 'one-on-one/:chatId',
+    //     element: <ChatMessageArea />,
+    //   },
+    //   {
+    //     path: 'group/:chatId',
+    //     element: <ChatMessageArea />,
+    //   },
+    // ],
   },
 ];
