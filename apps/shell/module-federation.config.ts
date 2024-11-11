d171155b9d74dee572789b9b93b5bc2fa017ab51
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-11 12:44:19 +0530

    feat: add remotes to shell app

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -14,7 +14,22 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: [],
+  remotes: [
+    ['auth', 'https://social-media-auth-app.netlify.app'],
+    ['profile', 'https://profile-mfe.netlify.app'],
+    ['friends', 'https://friends-mfe.netlify.app'],
+    ['chat', 'https://chat-mfe.netlify.app'],
+    ['chatlist', 'https://chatlist-mfe.netlify.app'],
+  ],
+  shared: (library, defaultConfig) => {
+    if (library === '@social-media/evoke-ui') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    return defaultConfig;
+  },
 };
 
 export default config;
---
+++ apps/shell/src/remote.d.ts
@@ -0,0 +1,5 @@
+declare module 'auth/Module';
+declare module 'profile/Module';
+declare module 'friends/Module';
+declare module 'chatlist/Module';
+declare module 'chat/Module';
