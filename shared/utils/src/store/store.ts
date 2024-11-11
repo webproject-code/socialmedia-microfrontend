621e0595f9c1adbb90620205f919f099ebca2b3e
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-11 10:16:40 +0530

    cherry pick: bring auth-slice, store & schema from auth-mfe branch

---
+++ shared/utils/src/store/store.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand';
+import { AuthSlice, createAuthSlice } from './authSlice';
+import {
+  RegistrationFormSlice,
+  createRegistrationFormSlice,
+} from './registrationFormSlice';
+import { devtools, persist } from 'zustand/middleware';
+
+export const useStore = create<AuthSlice & RegistrationFormSlice>()(
+  devtools(
+    persist(
+      (...args) => ({
+        ...createAuthSlice(...args),
+        ...createRegistrationFormSlice(...args),
+      }),
+      { name: 'social-media-store' }
+    ),
+    { name: 'social-media-store' }
+  )
+);
