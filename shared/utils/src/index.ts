5783481c81bc80bca9749c3633b26b9cd267e19a
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-12 13:49:11 +0530

    feat: add ErrorBoundry & ErrorFallback component

--- shared/utils/src/index.ts
+++ shared/utils/src/index.ts
@@ -2,6 +2,8 @@ export * from './components/Spinner';
 export * from './components/ThemeToggle';
 export * from './components/StatusMessageBox';
 export * from './components/NotFoundPage';
+export * from './components/ErrorBoundary';
+export * from './components/ErrorFallback';
 
 export * from './context/ThemeContext';
 export * from './context/ReactQueryContext';
