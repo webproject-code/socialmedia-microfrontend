1e659f827035b032bd9dc26519e6a1d8cf5bd6d9
Vatsal Dave <vatsaldave2002@gmail.com>
2024-11-14 18:31:21 +0530

    update remote entry

--- apps/app_chat/src/remote-entry.ts
+++ apps/app_chat/src/remote-entry.ts
@@ -1,5 +1,19 @@
 import Chats from './pages/Chats';
 import OneOnOneChat from './pages/OneOnOneChat';
 import GroupChat from './pages/GroupChat';
+import { useChatQuery } from './hooks/useChatQuery';
+import { useChatScroll } from './hooks/useChatScroll';
+import { useChatSocket } from './hooks/useChatSocket';
+import { useMessagesSearch } from './hooks/useMessagesSearch';
+import { useTypingStatus } from './hooks/useTypingStatus';
 
-export { Chats, OneOnOneChat, GroupChat };
+export {
+  Chats,
+  OneOnOneChat,
+  GroupChat,
+  useChatQuery,
+  useChatScroll,
+  useChatSocket,
+  useMessagesSearch,
+  useTypingStatus,
+};
