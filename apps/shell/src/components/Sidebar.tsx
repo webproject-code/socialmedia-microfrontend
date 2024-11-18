ff0fe47d6643fd548916a2df11b451db3fe3b2b0
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-18 13:59:44 +0530

    refactor: change icon selection logic and improve style of Sidebar

--- apps/shell/src/components/Sidebar.tsx
+++ apps/shell/src/components/Sidebar.tsx
@@ -14,40 +14,43 @@ const Sidebar: React.FC = () => {
   const navItems = [
     {
       to: '/',
-      icon: <FaHome className="w-8 h-8 md:w-10 md:h-10" />,
+      icon: <FaHome className="w-6 h-6 md:w-8 md:h-8" />,
       label: 'Home',
+      exact: true, // Ensure exact matching for Home
     },
     {
       to: '/chats',
-      icon: <IoChatbubble className="w-8 h-8 md:w-10 md:h-10" />,
+      icon: <IoChatbubble className="w-6 h-6 md:w-8 md:h-8" />,
       label: 'Chats',
     },
     {
       to: '/friends',
-      icon: <FaUserGroup className="w-8 h-8 md:w-10 md:h-10" />,
+      icon: <FaUserGroup className="w-6 h-6 md:w-8 md:h-8" />,
       label: 'Friends',
     },
     {
       to: `/users/${user.id}`,
-      icon: <FaUser className="w-8 h-8 md:w-10 md:h-10" />,
+      icon: <FaUser className="w-6 h-6 md:w-8 md:h-8" />,
       label: 'Profile',
     },
   ];
 
   return (
-    <aside className="flex w-full md:w-24 flex-row md:flex-col items-center p-4 md:py-8 md:space-y-6 border-t md:border-t-0 md:border-r border-gray-300 dark:border-gray-700">
-      <img
-        src="assets/Logo.svg"
-        alt="logo"
-        className="hidden md:block w-12 h-12 mb-4"
-      />
+    <aside className="flex w-full md:w-24 flex-row md:flex-col items-center p-2 md:p-4 md:py-8 md:space-y-6 border-t md:border-t-0 md:border-r border-gray-300 dark:border-gray-700 bg-light-modalColor/50 dark:bg-dark-modalColor/50">
+      <Link to={'/'}>
+        <img
+          src="assets/Logo.svg"
+          alt="logo"
+          className="hidden md:block w-12 h-12 mb-4"
+        />
+      </Link>
       <ul className="flex flex-grow flex-row md:flex-col w-full items-center justify-around md:space-y-8 md:justify-center">
-        {navItems.map(({ to, icon, label }) => (
+        {navItems.map(({ to, icon, label, exact }) => (
           <li key={to} className="relative group">
             <Link
               to={to}
               className={`${
-                pathname === to
+                (exact ? pathname === to : pathname.startsWith(to))
                   ? 'text-light-secondary dark:text-dark-secondary'
                   : 'text-gray-500 dark:text-gray-400'
               } group-hover:text-primary dark:group-hover:text-secondary`}
