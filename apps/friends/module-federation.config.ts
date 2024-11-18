fd45785cea5fe888939a1081f785761142b92e69
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-18 12:10:34 +0530

    fix: add shared library to module-federation.config.ts file

--- apps/friends/module-federation.config.ts
+++ apps/friends/module-federation.config.ts
@@ -6,6 +6,33 @@ const config: ModuleFederationConfig = {
   exposes: {
     './Module': './src/remote-entry.ts',
   },
+  shared: (library, defaultConfig) => {
+    if (library === '@social-media/evoke-ui') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    if (library === 'socket.io-client') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    if (library === '@hookform/resolvers/zod') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    if (library === '@tanstack/react-query') {
+      return {
+        ...defaultConfig,
+        strictVersion: false,
+      };
+    }
+    return defaultConfig;
+  },
 };
 
 export default config;
