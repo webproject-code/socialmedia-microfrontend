c6ed099c6073080ddce2d376339c11ba4e261467
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-29 18:13:44 +0530

    fix: upgrade modal usage

--- apps/app_chat/src/components/GroupChatInfoModal.tsx
+++ apps/app_chat/src/components/GroupChatInfoModal.tsx
@@ -17,7 +17,6 @@ import {
   Button,
   Input,
   Modal,
-  ScrollArea,
 } from '@social-media/evoke-ui';
 import {
   GroupSettingsFormData,
@@ -117,11 +116,7 @@ const GroupChatInfoModal: React.FC<GroupChatInfoModalProps> = ({
   if (groupChat && userData) {
     const isOwner = userData?.id === groupChat.ownerId;
     return (
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        className="dark:bg-dark-modalColor sm:max-w-[600px] max-h-[90vh] overflow-y-auto"
-      >
+      <Modal show={isOpen} onClose={onClose} size="sm">
         <Modal.Header className="text-2xl text-light-secondary dark:text-dark-lavender">
           <div>
             <h1>Group Information</h1>
@@ -130,137 +125,137 @@ const GroupChatInfoModal: React.FC<GroupChatInfoModalProps> = ({
             </div>
           </div>
         </Modal.Header>
-        <ScrollArea className="max-h-[90vh]">
-          <Modal.Content className="p-2 overflow-y-auto">
-            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-              <div className="flex items-center space-x-4">
-                <div className="relative">
-                  <Avatar size="lg" className="h-20 w-20">
-                    <AvatarImage
-                      src={previewUrl ? previewUrl : groupChat.groupIcon}
-                      className="ring-0"
-                    />
-                  </Avatar>
-                  {isOwner && (
-                    <label className="absolute bottom-0 right-0 p-1 bg-blue-600 rounded-full cursor-pointer">
-                      <FaCamera className="text-white" />
-                      <input
-                        type="file"
-                        accept="image/jpg, image/png"
-                        name="Group icon"
-                        onChange={handleIconChange}
-                        className="hidden"
-                      />
-                    </label>
-                  )}
-                </div>
-
-                <div className="flex-1 space-y-4">
-                  <div>
-                    <Controller
-                      name="name"
-                      control={control}
-                      render={({ field }) => (
-                        <Input
-                          {...field}
-                          label="Name"
-                          type="text"
-                          placeholder="Enter name"
-                          error={!!errors.name}
-                          errorMessage={errors.name?.message}
-                          autoComplete="off"
-                          disabled={!isOwner}
-                        />
-                      )}
-                    />
-                  </div>
-                  <div>
-                    <Controller
-                      name="groupDescription"
-                      control={control}
-                      render={({ field }) => (
-                        <Input
-                          {...field}
-                          label="Description"
-                          type="text"
-                          placeholder="Enter description"
-                          error={!!errors.groupDescription}
-                          errorMessage={errors.groupDescription?.message}
-                          autoComplete="off"
-                          disabled={!isOwner}
-                        />
-                      )}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center justify-end">
+        {/* <ScrollArea className="max-h-[90vh]"> */}
+        <Modal.Content className="p-2 overflow-y-auto">
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex items-center space-x-4">
+              <div className="relative">
+                <Avatar size="lg" className="h-20 w-20">
+                  <AvatarImage
+                    src={previewUrl ? previewUrl : groupChat.groupIcon}
+                    className="ring-0"
+                  />
+                </Avatar>
                 {isOwner && (
-                  <Button
-                    type="submit"
-                    className="w-fit px-4 py-2 justify-self-end dark:bg-dark-secondary"
-                    size="sm"
-                    disabled={
-                      updateSettingsMutation.isPending ||
-                      (previewUrl === null && !isDirty)
-                    }
-                  >
-                    {updateSettingsMutation.isPending
-                      ? 'Saving...'
-                      : 'Save Changes'}
-                  </Button>
+                  <label className="absolute bottom-0 right-0 p-1 bg-blue-600 rounded-full cursor-pointer">
+                    <FaCamera className="text-white" />
+                    <input
+                      type="file"
+                      accept="image/jpg, image/png"
+                      name="Group icon"
+                      onChange={handleIconChange}
+                      className="hidden"
+                    />
+                  </label>
                 )}
               </div>
-            </form>
 
-            <div className="mt-6">
-              <div className="space-y-4">
-                <div className="flex items-center justify-between">
-                  <h3 className="text-lg font-semifold text-light-secondary dark:text-dark-lavender">
-                    {isAddingMembersOpen
-                      ? 'Add Members'
-                      : `Members ${membersData?.members.length}`}
-                  </h3>
-                  {isOwner && !isAddingMembersOpen ? (
+              <div className="flex-1 space-y-4">
+                <div>
+                  <Controller
+                    name="name"
+                    control={control}
+                    render={({ field }) => (
+                      <Input
+                        {...field}
+                        label="Name"
+                        type="text"
+                        placeholder="Enter name"
+                        error={!!errors.name}
+                        errorMessage={errors.name?.message}
+                        autoComplete="off"
+                        disabled={!isOwner}
+                      />
+                    )}
+                  />
+                </div>
+                <div>
+                  <Controller
+                    name="groupDescription"
+                    control={control}
+                    render={({ field }) => (
+                      <Input
+                        {...field}
+                        label="Description"
+                        type="text"
+                        placeholder="Enter description"
+                        error={!!errors.groupDescription}
+                        errorMessage={errors.groupDescription?.message}
+                        autoComplete="off"
+                        disabled={!isOwner}
+                      />
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-end">
+              {isOwner && (
+                <Button
+                  type="submit"
+                  className="w-fit px-4 py-2 justify-self-end dark:bg-dark-secondary"
+                  size="sm"
+                  disabled={
+                    updateSettingsMutation.isPending ||
+                    (previewUrl === null && !isDirty)
+                  }
+                >
+                  {updateSettingsMutation.isPending
+                    ? 'Saving...'
+                    : 'Save Changes'}
+                </Button>
+              )}
+            </div>
+          </form>
+
+          <div className="mt-6">
+            <div className="space-y-4">
+              <div className="flex items-center justify-between">
+                <h3 className="text-lg font-semifold text-light-secondary dark:text-dark-lavender">
+                  {isAddingMembersOpen
+                    ? 'Add Members'
+                    : `Members ${membersData?.members.length}`}
+                </h3>
+                {isOwner && !isAddingMembersOpen ? (
+                  <Button
+                    className="w-fit  dark:text-dark-secondary"
+                    variant="outline"
+                    size="sm"
+                    onClick={() => setIsAddingMembersOpen(true)}
+                  >
+                    Add Members
+                  </Button>
+                ) : (
+                  isOwner && (
                     <Button
                       className="w-fit  dark:text-dark-secondary"
                       variant="outline"
                       size="sm"
-                      onClick={() => setIsAddingMembersOpen(true)}
+                      onClick={() => setIsAddingMembersOpen(false)}
                     >
-                      Add Members
+                      Cancel
                     </Button>
-                  ) : (
-                    isOwner && (
-                      <Button
-                        className="w-fit  dark:text-dark-secondary"
-                        variant="outline"
-                        size="sm"
-                        onClick={() => setIsAddingMembersOpen(false)}
-                      >
-                        Cancel
-                      </Button>
-                    )
-                  )}
-                </div>
-                {isAddingMembersOpen ? (
-                  <AddMembersList
-                    onAddMembers={handleAddMembers}
-                    existingMemberIds={groupChat.memberIds}
-                    currentUserId={userData.id}
-                  />
-                ) : (
-                  <MembersList
-                    members={membersData?.members || []}
-                    ownerId={groupChat.ownerId}
-                    currentUserId={userData.id}
-                    onRemoveMember={handleRemoveMember}
-                  />
+                  )
                 )}
               </div>
+              {isAddingMembersOpen ? (
+                <AddMembersList
+                  onAddMembers={handleAddMembers}
+                  existingMemberIds={groupChat.memberIds}
+                  currentUserId={userData.id}
+                />
+              ) : (
+                <MembersList
+                  members={membersData?.members || []}
+                  ownerId={groupChat.ownerId}
+                  currentUserId={userData.id}
+                  onRemoveMember={handleRemoveMember}
+                />
+              )}
             </div>
-          </Modal.Content>
-        </ScrollArea>
+          </div>
+        </Modal.Content>
+        {/* </ScrollArea> */}
       </Modal>
     );
   }
--- apps/chatlist/src/components/createChatModal.tsx
+++ apps/chatlist/src/components/createChatModal.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Input, Modal } from '@social-media/evoke-ui';
+import { Box, Button, Input, Modal, ScrollArea } from '@social-media/evoke-ui';
 import { Spinner, useDebounce, useTheme } from '@social-media/utils';
 import { useCreateOneOnOneChat, useFriendsWithNoChat } from '@social-media/api';
 import { useCallback, useState } from 'react';
@@ -90,185 +90,177 @@ export const CreateChatModal: React.FC<CreateChatModalProps> = ({
 
   return (
     <Modal
-      closeOnOutsideClick
+      closeOnOverlayClick
       onClose={() => setIsModalOpen(false)}
-      showCross
-      size="lg"
-      className=""
-      scrollBehaviour
-      isOpen={isModalOpen}
+      show={isModalOpen}
       tabIndex={0}
+      size="sm"
     >
-      <div className="bg-white dark:bg-gray-900 rounded-sm shadow-xl max-w-2xl w-full mx-auto">
-        {/* <Modal.Header> */}
-        <div className="border-b border-gray-200 dark:border-gray-800 p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="text-xl font-semibold text-light-secondary dark:text-dark-lavender">
-              {groupChat ? 'Create Group Chat' : 'New Chat'}
-            </h2>
-            <button
-              onClick={() => setIsModalOpen(false)}
-              className="text-light-silverSteel/50 hover:text-light-silverSteel dark:text-dark-silverSteel/50 dark:hover:text-dark-silverSteel"
-            >
-              <RxCross2 className="w-5 h-5" />
-            </button>
-          </div>
+      {/* <Modal.Header> */}
+      <div className="border-b border-gray-200 dark:border-gray-800 p-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold text-light-secondary dark:text-dark-lavender">
+            {groupChat ? 'Create Group Chat' : 'New Chat'}
+          </h2>
+          <button
+            onClick={() => setIsModalOpen(false)}
+            className="text-light-silverSteel/50 hover:text-light-silverSteel dark:text-dark-silverSteel/50 dark:hover:text-dark-silverSteel"
+          >
+            <RxCross2 className="w-5 h-5" />
+          </button>
         </div>
-        {/* </Modal.Header> */}
-        <Modal.Content className="h-96">
-          <div className="p-4">
-            {!groupChat && (
-              <Button
-                onClick={() => {
-                  setGroupChat(!groupChat);
-                  setMemberIds([]);
-                }}
-                className="dark:text-dark-primary dark:bg-dark-secondary focus-visible:ring-2
+      </div>
+      {/* </Modal.Header> */}
+      <Modal.Content>
+        {!groupChat && (
+          <Button
+            onClick={() => {
+              setGroupChat(!groupChat);
+              setMemberIds([]);
+            }}
+            className="dark:text-dark-primary dark:bg-dark-secondary focus-visible:ring-2
       focus-visible:ring-light-secondary
       focus-visible:ring-offset-2
       dark:focus-visible:ring-dark-secondary
       dark:focus-visible:ring-offset-dark-primary
       outline-none"
-                variant={'solid'}
-              >
-                Create Group
-              </Button>
-            )}
+            variant={'solid'}
+          >
+            Create Group
+          </Button>
+        )}
 
-            {groupChat && groupChatStep === 1 && (
+        {groupChat && groupChatStep === 1 && (
+          <button
+            className="cursor-pointer text-light-secondary dark:text-dark-secondary "
+            aria-label="previous step"
+            onClick={handleGroupChatPrevStep}
+          >
+            <FaCircleArrowLeft className="  w-6 h-6 " />
+          </button>
+        )}
+
+        {/* show selected group members */}
+        {groupChat && groupChatStep === 0 && (
+          <div className="space-y-3">
+            <div className="flex items-center justify-between">
+              <span className="text-sm text-gray-500 dark:text-gray-400">
+                {memberIds.length === 0
+                  ? 'Select Members'
+                  : `${memberIds.length} selected`}
+              </span>
+
               <button
-                className="cursor-pointer text-light-secondary dark:text-dark-secondary "
-                aria-label="previous step"
-                onClick={handleGroupChatPrevStep}
+                className="text-light-secondary dark:text-dark-secondary  disabled:text-light-silverSteel disabled:dark:text-dark-silverSteel disabled:cursor-not-allowed"
+                disabled={memberIds.length === 0}
+                aria-label="next step"
+                onClick={handleGroupChatNextStep}
               >
-                <FaCircleArrowLeft className="  w-6 h-6 " />
+                <FaCircleArrowRight className="w-6 h-6" />
               </button>
-            )}
+            </div>
 
-            {/* show selected group members */}
-            {groupChat && groupChatStep === 0 && (
-              <div className="space-y-3">
-                <div className="flex items-center justify-between">
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    {memberIds.length === 0
-                      ? 'Select Members'
-                      : `${memberIds.length} selected`}
-                  </span>
-
-                  <button
-                    className="text-light-secondary dark:text-dark-secondary  disabled:text-light-silverSteel disabled:dark:text-dark-silverSteel disabled:cursor-not-allowed"
-                    disabled={memberIds.length === 0}
-                    aria-label="next step"
-                    onClick={handleGroupChatNextStep}
+            {memberIds.length > 0 && (
+              <div className="flex flex-wrap gap-2">
+                {memberIds.map((member) => (
+                  <div
+                    key={member.id}
+                    className="group font-primary flex items-center gap-2 bg-blue-50 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 px-3 py-1.5 rounded-full text-sm"
                   >
-                    <FaCircleArrowRight className="w-6 h-6" />
-                  </button>
-                </div>
-
-                {memberIds.length > 0 && (
-                  <div className="flex flex-wrap gap-2">
-                    {memberIds.map((member) => (
-                      <div
-                        key={member.id}
-                        className="group font-primary flex items-center gap-2 bg-blue-50 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 px-3 py-1.5 rounded-full text-sm"
-                      >
-                        <span>{member.name}</span>
-                        <button
-                          type="button"
-                          onClick={() => removeMember(member.id)}
-                          className="opacity-60 hover:opacity-100 transition-opacity"
-                          aria-label={`Remove ${member.name}`}
-                        >
-                          <RxCross2 className="w-4 h-4" />
-                        </button>
-                      </div>
-                    ))}
+                    <span>{member.name}</span>
+                    <button
+                      type="button"
+                      onClick={() => removeMember(member.id)}
+                      className="opacity-60 hover:opacity-100 transition-opacity"
+                      aria-label={`Remove ${member.name}`}
+                    >
+                      <RxCross2 className="w-4 h-4" />
+                    </button>
                   </div>
-                )}
+                ))}
               </div>
             )}
-
-            {groupChat && groupChatStep === 1 && (
-              <CreateGroupChatForm
-                ownerId={currentUserId}
-                memberList={memberIds}
-                setMemberList={setMemberIds}
-                onCancel={onGroupChatCancel}
-                closeModal={setIsModalOpen}
-              />
-            )}
-
-            {groupChatStep === 0 && (
-              <>
-                <div className="relative my-2 mt-6">
-                  <Input
-                    type="text"
-                    name="search"
-                    value={searchTerm}
-                    onChange={handleChange}
-                    placeholder={'Search Here...'}
-                    aria-label="Search chats"
-                  >
-                    <LuSearch />
-                  </Input>
-                </div>
-
-                <div className="mt-4">
-                  {isLoading ? (
-                    <Box className="flex flex-col h-full w-full px-2">
-                      <FriendListSkeleton />
-                    </Box>
-                  ) : friends?.length === 0 ? (
-                    <Box className="flex justify-center items-center flex-col text-light-silverSteel/50 dark:text-dark-silverSteel/50">
-                      <img
-                        src={
-                          isDarkTheme
-                            ? 'assets/Images/dark-no-results-found-image.svg'
-                            : 'assets/Images/light-no-results-found-image.svg'
-                        }
-                        width={300}
-                        height={300}
-                        alt="search not found"
-                        className="object-fill h-[60%] w-[60%]"
-                      />
-                      <p className="font-primary">No User found !</p>
-                    </Box>
-                  ) : (
-                    <div className="space-y-1">
-                      {friends.map((user) => {
-                        // Check if user is selected as a group member
-                        const isMemberSelected = memberIds.some(
-                          (member) => member.id === user.id
-                        );
-                        return (
-                          <FriendCard
-                            selectedMember={isMemberSelected}
-                            key={user.id}
-                            name={user.name}
-                            email={user.email}
-                            profilePicture={user.profilePicture}
-                            onClickHandler={() =>
-                              handleCardClick(user.id, user.name)
-                            }
-                            groupChat={groupChat}
-                          />
-                        );
-                      })}
-                      <div ref={bottomRef}>
-                        {isFetchingNextPage && (
-                          <div className="flex justify-center items-center py-4">
-                            <Spinner />
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </>
-            )}
           </div>
-        </Modal.Content>
-      </div>
+        )}
+
+        {groupChat && groupChatStep === 1 && (
+          <CreateGroupChatForm
+            ownerId={currentUserId}
+            memberList={memberIds}
+            setMemberList={setMemberIds}
+            onCancel={onGroupChatCancel}
+            closeModal={setIsModalOpen}
+          />
+        )}
+
+        {groupChatStep === 0 && (
+          <>
+            <div className="relative my-2 mt-6">
+              <Input
+                type="text"
+                name="search"
+                value={searchTerm}
+                onChange={handleChange}
+                placeholder={'Search Here...'}
+                aria-label="Search chats"
+                icon={<LuSearch />}
+              />
+            </div>
+
+            <div className="mt-4">
+              {isLoading ? (
+                <Box className="flex flex-col h-full w-full px-2">
+                  <FriendListSkeleton />
+                </Box>
+              ) : friends?.length === 0 ? (
+                <Box className="flex justify-center items-center flex-col text-light-silverSteel/50 dark:text-dark-silverSteel/50">
+                  <img
+                    src={
+                      isDarkTheme
+                        ? 'assets/Images/dark-no-results-found-image.svg'
+                        : 'assets/Images/light-no-results-found-image.svg'
+                    }
+                    width={300}
+                    height={300}
+                    alt="search not found"
+                    className="object-fill h-[60%] w-[60%]"
+                  />
+                  <p className="font-primary">No User found !</p>
+                </Box>
+              ) : (
+                <ScrollArea className="h-[450px]">
+                  {friends.map((user) => {
+                    // Check if user is selected as a group member
+                    const isMemberSelected = memberIds.some(
+                      (member) => member.id === user.id
+                    );
+                    return (
+                      <FriendCard
+                        selectedMember={isMemberSelected}
+                        key={user.id}
+                        name={user.name}
+                        email={user.email}
+                        profilePicture={user.profilePicture}
+                        onClickHandler={() =>
+                          handleCardClick(user.id, user.name)
+                        }
+                        groupChat={groupChat}
+                      />
+                    );
+                  })}
+                  <div ref={bottomRef}>
+                    {isFetchingNextPage && (
+                      <div className="flex justify-center items-center py-4">
+                        <Spinner />
+                      </div>
+                    )}
+                  </div>
+                </ScrollArea>
+              )}
+            </div>
+          </>
+        )}
+      </Modal.Content>
     </Modal>
   );
 };
